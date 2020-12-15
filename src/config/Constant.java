c860724bb178d69b35a40d2c213a0d9ca5898d10
DuongSonn <duongson212998@gmail.com>
2020-12-16 00:41:26 +0700

    initial commit

---
+++ src/config/Constant.java
@@ -0,0 +1,17 @@
+package config;
+
+public class Constant {
+	public static String TOKEN = "1";
+	public static int OK = 1000;
+	public static int POST_NOT_FOUND = 9992;
+	public static int TOKEN_INVALID = 9998;
+	public static int IS_BANNED = 1;
+	public static int NOT_BANNED = 0;
+	public static int IS_BLOCKED = 1;
+	public static int CAN_COMMENT = 0;
+	public static String POST_NOT_FOUND_MSG = "Post can't be found";
+	public static String TOKEN_INVALID_MSG = "Token is invalid";
+	public static String POST_IS_BANNED = "Post is banned";
+	public static String POST_IS_BLOCKED = "Post is blocked";
+	public static String POST_ERROR = "Post has some errors";
+}
---
+++ src/models/Post.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Post {
+	private String token;
+	private String id;
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getToken() {
+		return token;
+	}
+	
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
